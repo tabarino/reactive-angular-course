248786d180725d828072ddd42626b1264ce97014
tabarino <tabarino@outlook.com>
2020-09-01 18:27:39 +0100

    Error Handling and the Messages Component

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,29 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
-import {Message} from '../model/message';
-import {tap} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Message } from '../model/message';
+import { tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+    selector: 'messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+    showMessages = false;
 
+    constructor() {
+    }
 
-  constructor() {
+    ngOnInit() {
+    }
 
-  }
-
-  ngOnInit() {
-
-
-  }
-
-
-  onClose() {
-
-
-  }
-
+    onClose() {
+        this.showMessages = false;
+    }
 }
