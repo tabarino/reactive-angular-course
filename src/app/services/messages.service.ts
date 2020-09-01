d17b26e00cf32fda801cc7725aee6e956d5e94ce
tabarino <tabarino@outlook.com>
2020-09-01 18:55:11 +0100

    Messages Service - Implementation Finished and Demo

--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Injectable()
 export class MessagesService {
+    private errSubject = new BehaviorSubject<string[]>([]);
+    errors$: Observable<string[]> = this.errSubject.asObservable()
+        .pipe(
+            filter(messages => messages && messages.length > 0)
+        );
+
     constructor() {
     }
 
     showErrors(...errors: string[]) {
-
+        this.errSubject.next(errors);
     }
 }
