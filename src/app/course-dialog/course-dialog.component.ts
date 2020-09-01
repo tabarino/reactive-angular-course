42fbeade69551a703403782eda1a5762073568c6
tabarino <tabarino@outlook.com>
2020-09-01 19:10:34 +0100

    Local Error Handling in an Angular Material Dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,12 +5,18 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import * as moment from 'moment';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../services/loading.service';
+import { MessagesService } from '../services/messages.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css'],
-    providers: [LoadingService]
+    providers: [
+        LoadingService,
+        MessagesService
+    ]
 })
 export class CourseDialogComponent implements AfterViewInit {
     form: FormGroup;
@@ -21,6 +27,7 @@ export class CourseDialogComponent implements AfterViewInit {
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         private coursesService: CoursesService,
         private loadingService: LoadingService,
+        private messagesService: MessagesService,
         @Inject(MAT_DIALOG_DATA) course: Course) {
 
         this.course = course;
@@ -38,7 +45,15 @@ export class CourseDialogComponent implements AfterViewInit {
 
     save() {
         const changes = this.form.value;
-        const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes);
+        const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes)
+            .pipe(
+                catchError(err => {
+                    const errMessage = 'Could not save course';
+                    this.messagesService.showErrors(errMessage);
+                    console.error(errMessage, err);
+                    return throwError(err);
+                })
+            );
 
         this.loadingService.showLoaderUntilCompleted(saveCourses$)
             .subscribe(val => {
