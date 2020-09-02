362ed8e67ba7c554ab36b91837335ab0aa63ffb6
tabarino <tabarino@outlook.com>
2020-09-02 20:45:32 +0100

    Angular Master Detail Implementation - The Detail Element

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -10,6 +10,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class SearchLessonsComponent implements OnInit {
     searchResults$: Observable<Lesson[]>;
+    activeLesson: Lesson;
 
     constructor(private coursesService: CoursesService) {
     }
@@ -19,5 +20,13 @@ export class SearchLessonsComponent implements OnInit {
 
     onSearch(search: string) {
         this.searchResults$ = this.coursesService.searchLessons(search);
+    }
+
+    openLesson(lesson: Lesson) {
+        this.activeLesson = lesson;
+    }
+
+    onBackToSearch() {
+        this.activeLesson = null;
     }
 }
