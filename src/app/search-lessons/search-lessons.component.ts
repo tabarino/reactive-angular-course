a8316e7db9739520694be93e9c3aeada291c5c81
tabarino <tabarino@outlook.com>
2020-09-02 20:38:46 +0100

    Angular Master Detail Implementation - The Master Table

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -1,48 +1,23 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat} from 'rxjs';
-import {Lesson} from '../model/lesson';
-
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Lesson } from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
-  selector: 'course',
-  templateUrl: './search-lessons.component.html',
-  styleUrls: ['./search-lessons.component.css']
+    selector: 'course',
+    templateUrl: './search-lessons.component.html',
+    styleUrls: ['./search-lessons.component.css']
 })
 export class SearchLessonsComponent implements OnInit {
+    searchResults$: Observable<Lesson[]>;
 
-  constructor() {
+    constructor(private coursesService: CoursesService) {
+    }
 
+    ngOnInit() {
+    }
 
-  }
-
-  ngOnInit() {
-
-
-  }
-
+    onSearch(search: string) {
+        this.searchResults$ = this.coursesService.searchLessons(search);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { map, shareReplay } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
     providedIn: 'root'
@@ -20,6 +21,18 @@ export class CoursesService {
 
     saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
         return this.http.put(`/api/courses/${ courseId }`, changes).pipe(
+            shareReplay()
+        );
+    }
+
+    searchLessons(search: string): Observable<Lesson[]> {
+        return this.http.get<Lesson[]>('/api/lessons', {
+            params: {
+                filter: search,
+                pageSize: '100'
+            }
+        }).pipe(
+            map(res => res['payload']),
             shareReplay()
         );
     }
